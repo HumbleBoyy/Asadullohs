5b5080749e423c9a37909c4729cc7ae2c3409b04
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-16 10:04:30 +0500

    File setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import myImage from "./assets/images/asadullohFive.jpg"
 const App = () => {
   return (
-    <div>
-      <img src={myImage} alt="Asadullohs" />
-    </div>
+    <>
+      Asadullohs
+    </>
   )
 }
 
---
+++ src/assets/images.js
@@ -0,0 +1,19 @@
+import one from './images/asadullohOne.jpg'
+import two from "./images/asadullohTwo.jpg"
+import three from "./images/asadullohThree.jpg"
+import four from "./images/asadullohFour.jpg"
+import five from "./images/asadullohFive.jpg"
+import six from "./images/asadullohSix.jpg"
+import seven from "./images/asadullohSeven.jpg"
+import eight from "./images/asadullohEight.jpg"
+
+export default{
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight
+}
