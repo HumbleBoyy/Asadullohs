a5c77969062d5873587baf067d49291bb6a7dec9
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-23 13:59:31 +0500

    Sending message to telegram

--- src/Components/Modal/Modales.jsx
+++ src/Components/Modal/Modales.jsx
@@ -8,6 +8,8 @@ import { FaTelegramPlane } from 'react-icons/fa';
 import { t } from 'i18next';
 import { IoSend } from 'react-icons/io5';
 import { MdCancelScheduleSend } from "react-icons/md";
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Modales = () => {
     const [open, setOpen] = useState(false);
@@ -22,13 +24,50 @@ const Modales = () => {
       border: '2px solid #000',
       boxShadow: 100,
       };
+
+      const sendMessage = (event) => {
+        event.preventDefault();
+        const token = "7725839839:AAHa2kiQaP6qKzbKONITba9S4lnzDFl9-0s";
+        const chat_id = 5560455535;
+        const url = `https://api.telegram.org/bot${token}/sendMessage`;
+        const name = document.getElementById("name").value;
+        const userName = document.getElementById("username").value;
+        const phone_num = document.getElementById("phone_num").value;
+        const message = document.getElementById("message").value;
+
+        const messageContent = `User Information\n
+         Name: ${name}\n
+         UserName: ${userName}\n
+         Phone: ${phone_num}\n
+         Message: ${message}\n
+        `
+
+        axios({
+          url: url,
+          method: "POST",
+          data: {
+              chat_id: chat_id,
+              text: messageContent,
+          }
+      })
+      .then((res) => {
+          toast.success(t("Contact.toast"));
+  
+          form.reset()
+  
+          handleClose();
+      })
+      .catch((error) => {
+          toast.error(t("Contact.toastError"));
+      });
+      }
   return (
     <>
 
     <section className='modal'>
         <div className="container">
             <div className="telegram_btn_wrapper">
-                <div className="telegram_btn" onClick={setOpen}>
+                <div className="telegram_btn" onClick={handleOpen}>
                   <FaTelegramPlane  className='telegram_icon'/>
                 </div>
             </div>
@@ -43,24 +82,24 @@ const Modales = () => {
       >
         <Box sx={style} className="Modal_box">
           <div className="contact_formModal">
-                    <form className="inputs_wrapper">
+                    <form className="inputs_wrapper" onSubmit={sendMessage}>
                     <div className="modal_header_title_wrapper">
                       <h2 className='modal_title'>{t("Modal.viaTelegram")}</h2>
                     </div>
                         <div className="inputBox">
-                            <input type="text" required="required" className="form_input modal_input"/>
+                            <input type="text" required="required" className="form_input modal_input" id='name'/>
                             <span className="form_span modal_span">{t("Contact.name")}</span>
                         </div>
                         <div className="inputBox">
-                            <input type="text" required="required" className="form_input modal_input"/>
+                            <input type="text" required="required" className="form_input modal_input" id='username'/>
                             <span className="form_span modal_span">{t("Modal.TelegramUsename")}</span>
                         </div>
                         <div className="inputBox">
-                            <input type="number" required="required" className="form_input modal_input"/>
+                            <input type="number" required="required" className="form_input modal_input" id='phone_num'/>
                             <span className="form_span modal_span">{t("Modal.phone")}</span>
                         </div>
                         <div className="inputBox">
-                            <textarea required="required" name="message" className="form_input modal_input" />
+                            <textarea required="required" name="message" className="form_input modal_input"  id='message'/>
                             <span className="form_span modal_span">{t("Contact.message")}</span>
                         </div>
                         <div className="inputBox modal_box_btns">
